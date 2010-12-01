1f603250fc451442705c4eced8b7f4e20b000b11
AlexBaranosky <alexander.baranosky@gmail.com>
2010-12-01 01:03:33 -0500

    added project files

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject fractal-tree "1.0.0-SNAPSHOT"
+  :description "Fractal tree drawing graphics program"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
