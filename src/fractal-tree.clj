e45e6b03e1c0a53ca710fe38281f26b4ed7754cd
AlexBaranosky <alexander.baranosky@gmail.com>
2010-12-01 01:38:13 -0500

    more cleanup!!!!!!

--- src/fractal-tree.clj
+++ src/fractal-tree.clj
@@ -2,7 +2,7 @@
   '(javax.swing JFrame JPanel)
   '(java.awt Color Graphics Graphics2D))
 
-(defn draw-tree [g2d angle x y length branch-angle depth]
+(defn- draw-tree [g2d angle x y length branch-angle depth]
   (if (> depth 0)
     (let [new-x (->> (Math/toRadians angle) Math/sin (* length) (- x))
           new-y (->> (Math/toRadians angle) Math/cos (* length) (- y))
@@ -12,7 +12,7 @@
       (draw-tree g2d (new-rand-angle +) new-x new-y (new-rand-length) branch-angle (- depth 1))
       (draw-tree g2d (new-rand-angle -) new-x new-y (new-rand-length) branch-angle (- depth 1)))))
 
-(defn render [g w h]
+(defn- render [g w h]
   (doto g
     (.setColor Color/BLACK)
     (.fillRect 0 0 w h)
@@ -22,14 +22,14 @@
         max-depth 12]
     (draw-tree g 0.0 (/ w 2) h init-length branch-angle max-depth)))
 
-(defn create-panel []
+(defn- create-panel []
   "Create a panel with a customised render"
   (proxy [JPanel] []
     (paintComponent [g]
       (proxy-super paintComponent g)
       (time (render g (.getWidth this) (.getHeight this))))))
 
-(defn run []
+(defn run-fractal-tree []
   (let [frame (JFrame. "Clojure Fractal Tree")
         panel (create-panel)]
     (doto frame
@@ -37,4 +37,4 @@
       (.setSize 640 400)
       (.setVisible true))))
 
-(run)
+(run-fractal-tree)
