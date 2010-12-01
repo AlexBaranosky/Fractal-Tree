a54968d9058f5d837115b9ca57f76d29d87fad47
AlexBaranosky <alexander.baranosky@gmail.com>
2010-12-01 01:45:29 -0500

    g

--- src/fractal-tree.clj
+++ src/fractal-tree.clj
@@ -2,15 +2,15 @@
   '(javax.swing JFrame JPanel)
   '(java.awt Color Graphics Graphics2D))
 
-(defn- draw-tree [g2d angle x y length branch-angle depth]
+(defn- draw-tree [g angle x y length branch-angle depth]
   (if (> depth 0)
     (let [new-x (->> (Math/toRadians angle) Math/sin (* length) (- x))
           new-y (->> (Math/toRadians angle) Math/cos (* length) (- y))
           new-rand-length #(->> (rand 0.1) (+ 0.75 ) (* length))
           new-rand-angle #(->> (rand) (+ 0.75) (* branch-angle) (% angle))]
-      (.drawLine g2d x y new-x new-y)
-      (draw-tree g2d (new-rand-angle +) new-x new-y (new-rand-length) branch-angle (- depth 1))
-      (draw-tree g2d (new-rand-angle -) new-x new-y (new-rand-length) branch-angle (- depth 1)))))
+      (.drawLine g x y new-x new-y)
+      (draw-tree g (new-rand-angle +) new-x new-y (new-rand-length) branch-angle (- depth 1))
+      (draw-tree g (new-rand-angle -) new-x new-y (new-rand-length) branch-angle (- depth 1)))))
 
 (defn- render [g w h]
   (doto g
